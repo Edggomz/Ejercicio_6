4ab28f9e322e3c89760d4d400176baa30ee46b2c
Edgar <20240109@ricaldone.edu.sv>
2025-06-13 22:47:29 -0600

    Ejercicio_6

--- Ejercicio 6/ejecicio 6/Form1.Designer.cs
+++ Ejercicio 6/ejecicio 6/Form1.Designer.cs
@@ -42,99 +42,116 @@
             // 
             // btnBorrar
             // 
+            this.btnBorrar.BackColor = System.Drawing.Color.Cornsilk;
             this.btnBorrar.Image = ((System.Drawing.Image)(resources.GetObject("btnBorrar.Image")));
             this.btnBorrar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnBorrar.Location = new System.Drawing.Point(617, 175);
+            this.btnBorrar.Location = new System.Drawing.Point(926, 215);
+            this.btnBorrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBorrar.Name = "btnBorrar";
-            this.btnBorrar.Size = new System.Drawing.Size(109, 76);
+            this.btnBorrar.Size = new System.Drawing.Size(164, 94);
             this.btnBorrar.TabIndex = 22;
             this.btnBorrar.Text = "Borrar";
             this.btnBorrar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.UseVisualStyleBackColor = false;
             this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
             // 
             // grbPerimetro
             // 
             this.grbPerimetro.Controls.Add(this.txtPerimetro);
             this.grbPerimetro.Controls.Add(this.lblPerimetro);
-            this.grbPerimetro.Location = new System.Drawing.Point(74, 292);
+            this.grbPerimetro.Location = new System.Drawing.Point(111, 359);
+            this.grbPerimetro.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grbPerimetro.Name = "grbPerimetro";
-            this.grbPerimetro.Size = new System.Drawing.Size(512, 100);
+            this.grbPerimetro.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grbPerimetro.Size = new System.Drawing.Size(768, 123);
             this.grbPerimetro.TabIndex = 21;
             this.grbPerimetro.TabStop = false;
             this.grbPerimetro.Text = "Resultado";
             // 
             // txtPerimetro
             // 
-            this.txtPerimetro.Location = new System.Drawing.Point(175, 46);
+            this.txtPerimetro.BackColor = System.Drawing.Color.Cornsilk;
+            this.txtPerimetro.Location = new System.Drawing.Point(262, 57);
+            this.txtPerimetro.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPerimetro.Name = "txtPerimetro";
             this.txtPerimetro.ReadOnly = true;
-            this.txtPerimetro.Size = new System.Drawing.Size(100, 20);
+            this.txtPerimetro.Size = new System.Drawing.Size(148, 22);
             this.txtPerimetro.TabIndex = 9;
             // 
             // lblPerimetro
             // 
             this.lblPerimetro.AutoSize = true;
-            this.lblPerimetro.Location = new System.Drawing.Point(23, 49);
+            this.lblPerimetro.Location = new System.Drawing.Point(34, 60);
+            this.lblPerimetro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPerimetro.Name = "lblPerimetro";
-            this.lblPerimetro.Size = new System.Drawing.Size(146, 13);
+            this.lblPerimetro.Size = new System.Drawing.Size(215, 16);
             this.lblPerimetro.TabIndex = 1;
             this.lblPerimetro.Text = "El Perimetro del Cuadrado es:";
             // 
             // btnSalir
             // 
+            this.btnSalir.BackColor = System.Drawing.Color.Cornsilk;
             this.btnSalir.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.Image")));
             this.btnSalir.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnSalir.Location = new System.Drawing.Point(617, 306);
+            this.btnSalir.Location = new System.Drawing.Point(926, 377);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(109, 76);
+            this.btnSalir.Size = new System.Drawing.Size(164, 94);
             this.btnSalir.TabIndex = 20;
             this.btnSalir.Text = "Salir";
             this.btnSalir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.UseVisualStyleBackColor = false;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // btnConvertir
             // 
+            this.btnConvertir.BackColor = System.Drawing.Color.Cornsilk;
             this.btnConvertir.Image = ((System.Drawing.Image)(resources.GetObject("btnConvertir.Image")));
             this.btnConvertir.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnConvertir.Location = new System.Drawing.Point(617, 58);
+            this.btnConvertir.Location = new System.Drawing.Point(926, 71);
+            this.btnConvertir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnConvertir.Name = "btnConvertir";
-            this.btnConvertir.Size = new System.Drawing.Size(109, 76);
+            this.btnConvertir.Size = new System.Drawing.Size(164, 94);
             this.btnConvertir.TabIndex = 19;
             this.btnConvertir.Text = "Convertir";
             this.btnConvertir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.UseVisualStyleBackColor = false;
             this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
             // 
             // lblLado
             // 
             this.lblLado.AutoSize = true;
-            this.lblLado.Location = new System.Drawing.Point(80, 145);
+            this.lblLado.Location = new System.Drawing.Point(120, 178);
+            this.lblLado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLado.Name = "lblLado";
-            this.lblLado.Size = new System.Drawing.Size(153, 13);
+            this.lblLado.Size = new System.Drawing.Size(220, 16);
             this.lblLado.TabIndex = 23;
             this.lblLado.Text = "Ingrese un Lado del Cuadrado:";
             // 
             // txtLado
             // 
-            this.txtLado.Location = new System.Drawing.Point(249, 142);
+            this.txtLado.BackColor = System.Drawing.Color.Cornsilk;
+            this.txtLado.Location = new System.Drawing.Point(374, 175);
+            this.txtLado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtLado.Name = "txtLado";
-            this.txtLado.Size = new System.Drawing.Size(100, 20);
+            this.txtLado.Size = new System.Drawing.Size(148, 22);
             this.txtLado.TabIndex = 24;
             // 
             // frmPerimetro
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(1200, 554);
             this.Controls.Add(this.txtLado);
             this.Controls.Add(this.lblLado);
             this.Controls.Add(this.btnBorrar);
             this.Controls.Add(this.grbPerimetro);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnConvertir);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmPerimetro";
             this.Text = "Calcular Perimetro";
             this.grbPerimetro.ResumeLayout(false);
